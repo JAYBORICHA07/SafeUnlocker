67f01bb4d74572932891db67819a8b717ebcf7c1
Jay Boricha <jayboricha707@gmail.com>
2023-07-16 13:51:32 +0530

    Tried node-qpdf and installing qpdf on the os too, with that it is working fine

--- backend/server.js
+++ backend/server.js
@@ -4,225 +4,7 @@ import multer from "multer";
 import cors from "cors";
 import { memoryStorage } from "multer";
 import { Readable  } from "stream";
-// import { exec } from 'child_process';
-// import pkg1 from 'fs';
-// const {readFileSync} = pkg1;
-// const {writeFileSync} = pkg1;
-
-
-
-// {// //start
-// async function extractAllPagesFromPDF(pdfPath, outputPdfPath, password) {
-//   const tempFolderPath = './temp'; // Temporary folder to store intermediate files
-
-//   // Create the temporary folder if it doesn't exist
-//   if (!existsSync(tempFolderPath)) {
-//     mkdirSync(tempFolderPath);
-//   }
-
-//   const tempOutputPrefix = `${tempFolderPath}/output`;
-//   const command = `pdfunite -upw ${password} ${pdfPath} ${tempOutputPrefix}`;
-  
-//   // Execute the command to remove password protection
-//   exec(command, async (error) => {
-//     if (error) {
-//       console.log('An error occurred:', error);
-//       return;
-//     }
-    
-//     const outputFilePath = `${tempOutputPrefix}.pdf`;
-//     const outputBuffer = readFileSync(outputFilePath);
-    
-//     // Write the extracted pages to the output PDF file
-//     writeFileSync(outputPdfPath, outputBuffer);
-    
-//     // Clean up temporary files
-//     unlinkSync(outputFilePath);
-//     rmdirSync(tempFolderPath);
-    
-//     console.log('Extraction complete!');
-//   });
-// }
-// //strt
-    
-// const pdfPath = '../inputfile/SEM 2 TERM FEES JAY BORICHA_protected.pdf';
-// const outputPdfPath = '../outputfile/SEM 2 TERM FEES JAY BORICHA_protected.pdf';
-
-// extractAllPagesFromPDF(pdfPath, outputPdfPath, password);
-
-
-// //end
-// start
-    // Read the password-protected PDF file
-    // const pdfPath = '../inputfile/SEM 2 TERM FEES JAY BORICHA_protected.pdf';
-    // const pdfBuffer = readFileSync(pdfPath);
-
-    // // Load the password-protected PDF ignoring encryption
-    // const srcDoc = await PDFDocument.load(pdfBuffer, { ignoreEncryption: true });
-
-    // // Create a new PDF document to store the extracted pages
-    // const extractedPdfDoc = await PDFDocument.create();
-
-    // // Copy and add all pages to the extracted PDF document
-    // const pagesToExtract = Array.from(Array(srcDoc.getPageCount()).keys());
-    // for (const pageNumber of pagesToExtract) {
-    //   const [copiedPage] = await extractedPdfDoc.copyPages(srcDoc, [pageNumber]);
-    //   extractedPdfDoc.addPage(copiedPage);
-    // }
-
-    // // Define the output file path and name
-    // const customFileName = file.originalname;
-    // const outputPath = `../outputfile/${customFileName}`;
-
-    // // Save the extracted PDF to the specified output file
-    // const extractedPdfBytes = await extractedPdfDoc.save();
-    // writeFileSync(outputPath, extractedPdfBytes);
-
-    // // end
-// import { PDFDocument, StandardFonts, PDFName} from 'pdf-lib';
-// import pkg from 'fs';
-// const {readFileSync} = pkg;
-// const {writeFileSync} = pkg;
-// //start
-// const pdfPath = '../inputfile/Brijrajsinh Jadeja-2_protected.pdf';
-// const pdfBuffer = readFileSync(pdfPath);
-
-
-// const pdfDoc = await PDFDocument.load(pdfBuffer, { ignoreEncryption: true });
-
-// const pageCount = pdfDoc.getPageCount();
-
-// const extractedPdfDoc = await PDFDocument.create();
-
-// const pagesToExtract = Array.from(Array(pdfDoc.getPageCount()).keys());
-//   for (const pageNumber of pagesToExtract) {
-//     const [copiedPage] = await extractedPdfDoc.copyPages(pdfDoc, [pageNumber]);
-
-//     const contentStream = copiedPage.getOperators();
-//     contentStream.add(PDFName.of('CA'));
-//     contentStream.push(1);
-//     contentStream.add(PDFName.of('ca'));
-//     contentStream.push(1);
-
-
-//     const font = await extractedPdfDoc.embedFont(StandardFonts.Helvetica);
-//     copiedPage.setFont(font);
-//     extractedPdfDoc.addPage(copiedPage);
-  
-
-//     extractedPdfDoc.addPage(copiedPage);
-//   }
-
-// const customFileName = file.originalname;
-// const extractedPdfBytes = await extractedPdfDoc.save();
-// const outputPath = `../outputfile/${customFileName}`;
-
-// writeFileSync(outputPath, extractedPdfBytes);
-
-// //end
-//end}
-
-// {
-  //
-  // async function extractAllPagesFromPDF(pdfPath, outputPdfPath, password) {
-  //   const pdfData = new Uint8Array(readFileSync(pdfPath));
-  
-  //   const loadingTask = getDocument({
-  //     data: pdfData,
-  //     password: password,
-  //   });
-  
-  //   try {
-  //     const pdf = await loadingTask.promise;
-  //     const pageCount = pdf.numPages;
-  
-  //     const pdfWriter = await getDocument({}).promise;
-  
-  //     for (let pageNumber = 1; pageNumber <= pageCount; pageNumber++) {
-  //       const page = await pdf.getPage(pageNumber);
-  
-  //       const scale = 1.0;
-  //       const viewport = page.getViewport({ scale });
-  
-  //       const canvasFactory = new DOMCanvasFactory();
-  //       const canvasAndContext = canvasFactory.create(
-  //         viewport.width,
-  //         viewport.height
-  //       );
-  
-  //       const renderContext = {
-  //         canvasContext: canvasAndContext.context,
-  //         viewport: viewport,
-  //         canvasFactory: canvasFactory,
-  //       };
-  
-  //       await page.render(renderContext);
-  
-  //       const imageData = canvasAndContext.canvas.toDataURL();
-  //       const imageDataBytes = atob(imageData.split(',')[1]);
-  //       const arrayBuffer = new ArrayBuffer(imageDataBytes.length);
-  //       const data = new Uint8Array(arrayBuffer);
-  
-  //       for (let i = 0; i < imageDataBytes.length; i++) {
-  //         data[i] = imageDataBytes.charCodeAt(i);
-  //       }
-  
-  //       const img = await pdfWriter.createImage(data);
-  //       const imgDims = { width: viewport.width, height: viewport.height };
-  //       const pageObj = await pdfWriter.createPage(img, imgDims);
-  //       pdfWriter.addPage(pageObj);
-  //     }
-  
-  //     const newPdfBytes = await pdfWriter.save();
-  //     writeFileSync(outputPdfPath, newPdfBytes);
-  //     console.log('Extraction complete!');
-  //   } catch (error) {
-  //     console.log('An error occurred:', error);
-  //   }
-  // }
-  //
-  // import pkg from 'pdfjs-dist';
-  // const {getDocument} = pkg;
-  // const {getPage} = pkg
-// function extractTextFromPage(page) {
-//   return page.getTextContent()
-//     .then(function (textContent) {
-//       var extractedText = '';
-//       for (var i = 0; i < textContent.items.length; i++) {
-//         extractedText += textContent.items[i].str + ' ';
-//       }
-//       console.log(extractedText);
-//       return extractedText;
-//     });
-// }
-
-// function extractTextFromAllPages(pdf) {
-//   var totalPageCount = pdf.numPages;
-//   console.log(totalPageCount);
-//   var pagesTextPromises = [];
-
-//   for (var pageNum = 1; pageNum <= totalPageCount; pageNum++) {
-//     pagesTextPromises.push(
-
-//       pdf.getPage(pageNum)
-//         .then(extractTextFromPage)
-//     );
-//   }
-
-//   return Promise.all(pagesTextPromises);
-// }
-// // Start
-    
-// getDocument({ url: '../inputfile/Brijrajsinh Jadeja-2_protected.pdf', password: password }).promise
-// .then(extractTextFromAllPages)
-// .then(function (extractedTextArray) {
-//   console.log('Extracted text:', extractedTextArray);
-// })
-// .catch(function (error) {
-//   console.error('Error occurred while extracting text:', error);
-// });
-// // End
-// }
+import { decrypt } from "node-qpdf2";
 
 const app = express();
 app.use(cors());
@@ -230,11 +12,6 @@ app.use(cors());
 // Multer storage configuration
 const storage = memoryStorage();
 const upload = multer({ storage });
-
-
-
-
-
 
 // Configuration for Google Drive API
 const auth = new google.auth.GoogleAuth({
@@ -253,8 +30,16 @@ app.post("/upload-to-google-drive", upload.single("file"), async (req, res) => {
 
     let file = req.file;
     const password = req.body.password;
-    
-    
+    const customFileName = file.originalname;
+    const outputPath = `../outputfile/${customFileName}`;
+    const options = {
+      input: "../inputfile/SEM 2 TERM FEES JAY BORICHA_protected.pdf",
+      output: `../outputfile/${customFileName}`,
+      password: password,
+    }
+
+
+    await decrypt(options);
 
     const folderId = "1XVTBP1njQ8d3OarbVTCqJDjly0YNChRo"; 
     const media = {
